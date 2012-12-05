c7dbedd94b8afeb8fc36c01a5bc4aaf344ee6c1a
Bernhard Froehler <bernhard.froehler@gmx.at>
2012-12-06 00:24:25 +0100

    Changes to make extension fit for publishing

--- bfstop.php
+++ bfstop.php
@@ -1,22 +1,11 @@
 <?php
-defined('_JEXEC') or die;
 /*
-Brute Force Stop (bfstop) Joomla Plugin
-Copyright (C) 2012 Bernhard Froehler
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * @package Brute Force Stop (bfstop) for Joomla! >=2.5
+ * @author Bernhard Froehler
+ * @copyright (C) 2012 Bernhard Froehler
+ * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+**/
+defined('_JEXEC') or die;
 
 jimport('joomla.event.plugin');
 jimport('joomla.error.log');
