1bc3f263f80d2e810698a14df99b1066ec637953
Bernhard Froehler <bernhard@bfroehler.info>
2013-01-06 17:43:16 +0100

    Corrects uninstall script to actually work

--- sql/uninstall.mysql.utf8.sql
+++ sql/uninstall.mysql.utf8.sql
@@ -1,6 +1,6 @@
 -- uninstall script for bfstop plugin
 
-DROP TABLE IF EXISTS #__bfstop_failedlogin
+DROP TABLE IF EXISTS `#__bfstop_failedlogin`;
 
-DROP TABLE IF EXISTS #__bfstop_bannedip
+DROP TABLE IF EXISTS `#__bfstop_bannedip`;
 
