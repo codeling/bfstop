b60d9da97b289bc35b2b9e144049b439eafb949f
solrac <comproperty247@gmail.com>
2013-05-04 07:43:02 +0100

    adding defined( '_JEXEC' ) or die;

--- helper.log.php
+++ helper.log.php
@@ -1,4 +1,5 @@
 <?php
+defined( '_JEXEC' ) or die;
 class BFStopLogger {
 
         private $enabled;
--- helper.notify.php
+++ helper.notify.php
@@ -1,5 +1,4 @@
 <?php
- // no direct access
 defined( '_JEXEC' ) or die;
 class BFStopNotifier
 {
