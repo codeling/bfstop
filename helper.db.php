74e8ddd23d67f905678d3a40386a4c12eed60de4
solrac <comproperty247@gmail.com>
2013-05-04 07:44:29 +0100

    Avoid full scan on users table

--- helper.db.php
+++ helper.db.php
@@ -1,4 +1,5 @@
 <?php
+defined( '_JEXEC' ) or die;
 class BFStopDBHelper {
 
 	private $db;
@@ -128,7 +129,7 @@ class BFStopDBHelper {
 
 	private function getUserEmailWhere($where)
 	{
-		$sql = "select email from #__users where $where";
+		$sql = "select email from #__users where $where LIMIT 1";
 		$this->db->setQuery($sql);
 		$emailAddress = $this->db->loadResult();
 		$this->checkDBError();
