84c029518c3a159671f656c1a3a6919dd4ff1edd
Bernhard Froehler <bernhard@bfroehler.info>
2017-01-29 11:38:09 +0100

    During update to 1.4.0, keep date entries of whitelist in notes

--- sql/updates/1.4.0.sql
+++ sql/updates/1.4.0.sql
@@ -1,5 +1,8 @@
 ALTER TABLE `#__bfstop_failedlogin` DROP COLUMN error;
 
-ALTER TABLE `#__bfstop_whitelist` DROP COLUMN crdate;
 
 ALTER TABLE `#__bfstop_whitelist` ADD COLUMN notes varchar(255) NOT NULL DEFAULT '';
+
+UPDATE `#__bfstop_whitelist` SET notes=CONCAT('created: ',DATE_FORMAT(crdate, '%Y-%m-%d')) WHERE crdate != '0000-00-00 00:00:00';
+
+ALTER TABLE `#__bfstop_whitelist` DROP COLUMN crdate;
