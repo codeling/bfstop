f9e46395ffed2ac8f806065ac4088241b0f4c77b
Bernhard Froehler <bernhard@bfroehler.info>
2017-09-16 22:38:15 +0200

    Update headers (fixes JED checker warnings)

--- helpers/notify.php
+++ helpers/notify.php
@@ -1,8 +1,8 @@
 <?php
 /*
- * @package Brute Force Stop (bfstop) for Joomla! >=2.5
+ * @package BFStop Plugin (bfstop) for Joomla! >=2.5
  * @author Bernhard Froehler
- * @copyright (C) 2012-2014 Bernhard Froehler
+ * @copyright (C) 2012-2017 Bernhard Froehler
  * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
 **/
 defined( '_JEXEC' ) or die;
