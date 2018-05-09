1adba1ad5bd56a0e865650b049cdb3049c8f9093
Bernhard Froehler <bernhard@bfroehler.info>
2018-05-09 22:07:30 +0200

    Change copyright note to include 2018

--- helpers/notify.php
+++ helpers/notify.php
@@ -2,7 +2,7 @@
 /*
  * @package BFStop Plugin (bfstop) for Joomla! >=2.5
  * @author Bernhard Froehler
- * @copyright (C) 2012-2017 Bernhard Froehler
+ * @copyright (C) 2012-2018 Bernhard Froehler
  * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
 **/
 defined( '_JEXEC' ) or die;
