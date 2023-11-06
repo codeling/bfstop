f80cb80a22056f80d55cf521bfae1799880d7b47
Bernhard Froehler <bernhard@bfroehler.info>
2023-11-06 23:30:13 +0100

    JED checker: add missing file header

--- updatescript.php
+++ updatescript.php
@@ -1,4 +1,10 @@
 <?php
+/*
+ * @package BFStop Plugin (bfstop) for Joomla! >=2.5
+ * @author Bernhard Froehler
+ * @copyright (C) Bernhard Froehler
+ * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+**/
 defined('_JEXEC') or die;
 
 class PlgsystembfstopInstallerScript
