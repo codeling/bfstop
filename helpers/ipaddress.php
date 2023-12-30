5e5c24ba0750ae2dc66a96fe61fd32c1c1e82154
Bernhard Froehler <bernhard@bfroehler.info>
2023-12-30 22:45:07 +0100

    JED checker: Add missing JEXEC security check

--- helpers/ipaddress.php
+++ helpers/ipaddress.php
@@ -1,10 +1,11 @@
 <?php
 /*
- * @package BFStop Component (com_bfstop) for Joomla!
+ * @package BFStop Plugin (bfstop) for Joomla!
  * @author Bernhard Froehler
  * @copyright (C) Bernhard Froehler
  * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
 **/
+defined('_JEXEC') or die;
 
 function getIPAddr($logger)
 {
